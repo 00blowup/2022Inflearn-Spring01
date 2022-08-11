296341ac1af850e648699a23a4c1b0d39e55a845
Haeun Noh <00blowup@gmail.com>
2022-08-12 03:53:47 +0900

    JPA 관련 commit

--- hello-spring/src/main/java/hello/hellospring/domain/Member.java
+++ hello-spring/src/main/java/hello/hellospring/domain/Member.java
@@ -1,8 +1,13 @@
 package hello.hellospring.domain;
 
+import javax.persistence.*;
+
+@Entity
 public class Member {
 
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;    //고객이 지정해서 로그인할 때 쓰는 그 ID가 아니고, 시스템이 고객 구별을 위해 부여하는 고유값 ID임!
+
     private String name;
 
     public Long getId() {
